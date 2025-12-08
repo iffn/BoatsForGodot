5872b006bf9534f8fb3bee28034f5398ab5d9751
iffn <iffn1990@gmail.com>
2025-12-08 17:13:52 +0100

    Moved script into its own class with perimiter

--- Water/RestrictedQuadTreeGenerator.gd
+++ Water/RestrictedQuadTreeGenerator.gd
@@ -1,5 +1,6 @@
 extends MeshInstance3D
 
+## Configuration
 @export var plane_size: float = 1.0
 @export_range(1, 100) var grid_resolution: int = 10 
 
@@ -7,47 +8,103 @@ extends MeshInstance3D
 	set(value):
 		generate_mesh()
 
-var surface_tool: SurfaceTool
+var _surface_tool: SurfaceTool
 
 func generate_mesh() -> void:
-	# 1. Initialize SurfaceTool
-	surface_tool = SurfaceTool.new()
-	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	# Initialize SurfaceTool
+	_surface_tool = SurfaceTool.new()
+	_surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
-	var half_size = plane_size / 2.0
-	var step_size = plane_size / float(grid_resolution)
+	# Instantiate and run the generator class (generation happens in _init)
+	var generator = UniformGridGenerator.new(
+		plane_size, 
+		grid_resolution
+	)
 	
-	var vertex_count_per_side = grid_resolution + 1
-
-	# Generate Vertices
-	for z_index in range(vertex_count_per_side):
-		for x_index in range(vertex_count_per_side):
-			var x = float(x_index) * step_size - half_size
-			var z = float(z_index) * step_size - half_size
-			
-			var vertex = Vector3(x, 0.0, z)
-			surface_tool.add_vertex(vertex)
-
-	# Generate Indices
-	for z_quad in range(grid_resolution):
-		for x_quad in range(grid_resolution):
-			var i_tl = (z_quad * vertex_count_per_side) + x_quad
-			
-			var v0 = i_tl                         
-			var v1 = i_tl + 1                     
-			var v2 = i_tl + vertex_count_per_side 
-			var v3 = i_tl + vertex_count_per_side + 1 
-
-			# Triangulation (CW for upwards normal)
-			surface_tool.add_index(v3)
-			surface_tool.add_index(v2)
-			surface_tool.add_index(v0)
-			
-			surface_tool.add_index(v1)
-			surface_tool.add_index(v3)
-			surface_tool.add_index(v0)
+	# Debug perimiter. Incease scale to test
+	var debug_scale := 0.0
+	for i in range(generator.perimeter_indices.size()):
+		generator.vertices[generator.perimeter_indices[i]].y += i * debug_scale
+	
+	# Transfer data from the class arrays to the SurfaceTool
+	for vertex in generator.vertices:
+		_surface_tool.add_vertex(vertex)
+		
+	for index in generator.indices:
+		_surface_tool.add_index(index)
 
 	# Finalize Mesh
-	surface_tool.generate_normals()
+	_surface_tool.generate_normals()
+	self.mesh = _surface_tool.commit()
+
+class UniformGridGenerator:
+	var plane_size: float
+	var grid_resolution: int
 	
-	self.mesh = surface_tool.commit()
+	# Public arrays to store the generated mesh data
+	var vertices: PackedVector3Array = []
+	var indices: PackedInt32Array = []
+	var perimeter_indices: PackedInt32Array = [] 
+	
+	var _half_size: float
+	var _step_size: float
+	var _vertex_count_per_side: int
+
+	# Constructor
+	func _init(size: float, resolution: int):
+		plane_size = size
+		grid_resolution = resolution
+		
+		_half_size = plane_size / 2.0
+		_step_size = plane_size / float(grid_resolution)
+		_vertex_count_per_side = grid_resolution + 1
+		
+		var n = _vertex_count_per_side
+		
+		# Vertices
+		for z_index in range(n):
+			for x_index in range(n):
+				var x = float(x_index) * _step_size - _half_size
+				var z = float(z_index) * _step_size - _half_size
+				
+				var vertex = Vector3(x, 0.0, z)
+				vertices.push_back(vertex)
+		
+		# Indices
+		for z_quad in range(grid_resolution):
+			for x_quad in range(grid_resolution):
+				var index_top_left = (z_quad * n) + x_quad
+				
+				var v0 = index_top_left
+				var v1 = index_top_left + 1
+				var v2 = index_top_left + n
+				var v3 = index_top_left + n + 1
+				
+				indices.push_back(v3)
+				indices.push_back(v2)
+				indices.push_back(v0)
+				
+				indices.push_back(v1)
+				indices.push_back(v3)
+				indices.push_back(v0)
+		
+		# Perimiter indices:
+		
+		# Top Edge (Left to Right)
+		for i in range(n):
+			perimeter_indices.push_back(i)
+			
+		# Right Edge (Top to Bottom)
+		for i in range(1, n):
+			# Index of the rightmost vertex in row i
+			var index = i * n + (n - 1)
+			perimeter_indices.push_back(index)
+			
+		# Bottom Edge (Right to Left)
+		for i in range(n * n - 2, n * n - n, -1):
+			perimeter_indices.push_back(i)
+			
+		# Left Edge (Bottom to Top)
+		for i in range(n - 1, 0, -1):
+			var index = i * n
+			perimeter_indices.push_back(index)
