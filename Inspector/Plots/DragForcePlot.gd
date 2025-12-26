d41e142de2329fdd6cf2c679dd685c35ddbbcfa5
iffn <iffn1990@gmail.com>
2025-12-26 13:37:25 +0100

    Implemented lift analysis

--- Inspector/Plots/DragForcePlot.gd
+++ Inspector/Plots/DragForcePlot.gd
@@ -1,27 +1,39 @@
 extends Node
 
 @export var calculation_boat : BoatController
-@export var chart: Chart
+@export var force_chart: Chart
+@export var position_chart: Chart
 
 var friction_drag_funciton : Function
 var pressure_drag_funciton : Function
 var total_drag_funciton : Function
+var lift_force_function : Function
+var level_position_function : Function
 
-var cp: ChartProperties = ChartProperties.new()
+var cp_forces: ChartProperties = ChartProperties.new()
+var cp_position: ChartProperties = ChartProperties.new()
 
 func _ready() -> void:
-	var x: Array[float] = [-180.0, 180.0]
+	var x: Array[float] = [0.0, 50.0]
 	var y: Array[float] = [0.0, 0.0]
 	
-	cp = EaysChartHelper.get_default_chart_values()
+	cp_forces = EaysChartHelper.get_default_chart_values()
+	cp_forces.title = "Forces at speed"
+	cp_forces.x_label = "Speed [m/s]"
+	cp_forces.y_label = "Force [N]"
+	cp_forces.x_scale = 10
+	cp_forces.y_scale = 10
+	cp_forces.interactive = true 
+	cp_forces.show_legend = true
 	
-	cp.title = "Drag force at speed"
-	cp.x_label = "Speed [m/s]"
-	cp.y_label = "Force [N]"
-	cp.x_scale = 10
-	cp.y_scale = 10
-	cp.interactive = true 
-	cp.show_legend = false
+	cp_position = EaysChartHelper.get_default_chart_values()
+	cp_position.title = "Position at speed"
+	cp_position.x_label = "Speed [m/s]"
+	cp_position.y_label = "Position [m]"
+	cp_position.x_scale = 10
+	cp_position.y_scale = 10
+	cp_position.interactive = true 
+	cp_position.show_legend = false
 	
 	friction_drag_funciton = Function.new(
 		x, y, "Friction drag [N]",
@@ -53,7 +65,28 @@ func _ready() -> void:
 		}
 	)
 	
-	chart.plot([friction_drag_funciton, pressure_drag_funciton, total_drag_funciton], cp)
+	lift_force_function = Function.new(
+		x, y, "Lift force [N]",
+		{ 
+			color = Color("9d00ffff"),
+			marker = Function.Marker.CIRCLE,
+			type = Function.Type.LINE,
+			interpolation = Function.Interpolation.LINEAR
+		}
+	)
+	
+	level_position_function = Function.new(
+		x, y, "Level position [m]",
+		{ 
+			color = Color("00ff00ff"),
+			marker = Function.Marker.CIRCLE,
+			type = Function.Type.LINE,
+			interpolation = Function.Interpolation.LINEAR
+		}
+	)
+	
+	force_chart.plot([friction_drag_funciton, pressure_drag_funciton, total_drag_funciton, lift_force_function], cp_forces)
+	position_chart.plot([level_position_function], cp_position)
 
 func plot_graph():
 	var original_position := calculation_boat.position
@@ -70,6 +103,10 @@ func plot_graph():
 	pressure_drag_funciton.__y.clear()
 	total_drag_funciton.__x.clear()
 	total_drag_funciton.__y.clear()
+	lift_force_function.__x.clear()
+	lift_force_function.__y.clear()
+	level_position_function.__x.clear()
+	level_position_function.__y.clear()
 	
 	for speed in range(0, 51, 1):
 		calculation_boat.linear_velocity = Vector3(0, 0, -speed)
@@ -80,10 +117,14 @@ func plot_graph():
 		friction_drag_funciton.add_point(speed, data.friction_drag_force.z)
 		pressure_drag_funciton.add_point(speed, data.pressure_drag_force.z)
 		total_drag_funciton.add_point(speed, (data.friction_drag_force + data.pressure_drag_force).z)
+		lift_force_function.add_point(speed, (data.friction_drag_force + data.pressure_drag_force).y)
+		level_position_function.add_point(speed, water_line)
 	
-	EaysChartHelper.set_min_max([friction_drag_funciton, pressure_drag_funciton, total_drag_funciton], chart)
+	EaysChartHelper.set_min_max([friction_drag_funciton, pressure_drag_funciton, total_drag_funciton, lift_force_function], force_chart)
+	EaysChartHelper.set_min_max([level_position_function], position_chart)
 	
-	chart.queue_redraw()
+	force_chart.queue_redraw()
+	position_chart.queue_redraw()
 	
 	# Reset
 	calculation_boat.position = original_position
