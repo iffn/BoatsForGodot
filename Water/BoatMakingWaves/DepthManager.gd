efc1ae8940c20307c9a412aaa305ebb3e9c91fa0
iffn <iffn1990@gmail.com>
2026-01-22 21:45:02 +0100

    Started implementing heightmap water

--- Water/BoatMakingWaves/DepthManager.gd
+++ Water/BoatMakingWaves/DepthManager.gd
@@ -6,6 +6,7 @@ extends Node3D
 @export var water_quad : Node3D
 @export var resolution := 512
 @export var size := 100.0
+@export var should_update_position := true
 
 var pixels_per_unit : float
 
@@ -24,6 +25,10 @@ func _ready() -> void:
 	simulation_shader.set_shader_parameter("initializationFactor", 0)
 
 func _process(_delta: float) -> void:
+	if(should_update_position):
+		update_position()
+
+func update_position(): 
 	var displacement := reference_boat.global_position - global_position
 	
 	var offset_x : int = int(round(displacement.x * pixels_per_unit))
