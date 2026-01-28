8354e5de0390c383e570db51283fcc7a37b043d4
iffn <iffn1990@gmail.com>
2026-01-28 23:54:56 +0100

    Added metadata to tree

--- Inspector/HierarchyTree.gd
+++ Inspector/HierarchyTree.gd
@@ -15,8 +15,33 @@ func render_tree(target_node: Node) -> void:
 func _create_tree_branch(node: Node, parent_item: TreeItem) -> void:
 	var new_item = create_item(parent_item)
 	new_item.set_text(0, node.name)
+	new_item.set_metadata(0, node.name)
+	for key in node.get_meta_list():
+		var val = node.get_meta(key)
+		if val is Dictionary:
+			var extras_root = create_item(new_item)
+			extras_root.set_text(0, key)
+			extras_root.set_selectable(0, false)
+			for sub_key in val:
+				var sub_val = val[sub_key]
+				var meta_item = create_item(extras_root)
+				var display_text = str(sub_key) + ": " + str(sub_val)
+				meta_item.set_text(0, display_text)
+				meta_item.set_metadata(0, display_text)
+		else:
+			var meta_item = create_item(new_item)
+			var display_text = str(key) + ": " + str(val)
+			meta_item.set_text(0, display_text)
+			meta_item.set_metadata(0, display_text)
+	
 	for child in node.get_children():
 		_create_tree_branch(child, new_item)
+
+func _gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_copy") or (event is InputEventKey and event.keycode == KEY_C and event.ctrl_pressed):
+		var selected = get_selected()
+		if selected:
+			DisplayServer.clipboard_set(selected.get_metadata(0))
 
 func _add_children_to_tree(parent_node: Node, parent_item: TreeItem) -> void:
 	for child in parent_node.get_children():
